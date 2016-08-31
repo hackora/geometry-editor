c7599d958081d23126b131547c53ae985057c87d
Rune Dalmo <rune.dalmo@gmail.com>
2016-08-31 14:29:40 +0200

    Added an FPS viewer.

--- gmlibscenequickfbo.cpp
+++ gmlibscenequickfbo.cpp
@@ -12,6 +12,8 @@ GMlibSceneQuickFbo::GMlibSceneQuickFbo() {
 
   connect( this, &QQuickItem::windowChanged,
            this, &GMlibSceneQuickFbo::onWindowChanged );
+
+  _prev_time = std_system_clock::now();
 }
 
 QQuickFramebufferObject::Renderer*
@@ -68,4 +70,33 @@ GMlibSceneQuickFbo::onWindowChanged(QQuickWindow* w) {
 
   connect( this,   &GMlibSceneQuickFbo::signWheelEventOccurred,
            window, &Window::signWheelEventOccurred );
+
+  connect( window, &Window::beforeRendering,
+           this,   &GMlibSceneQuickFbo::updateFps );
 }
+
+unsigned int
+GMlibSceneQuickFbo::fps() const {
+
+  return _fps_avg;
+}
+
+void 
+GMlibSceneQuickFbo::updateFps() {
+
+  using namespace std::chrono; 
+
+  auto current_time = system_clock::now();
+  auto since_prev   = duration_cast<milliseconds>(current_time -_prev_time);
+
+  const auto fps_interval = 2;
+  if(since_prev > seconds(fps_interval)) {
+    _prev_time   += seconds(fps_interval);
+    _fps_avg      = _fps_counter / fps_interval;
+    _fps_counter  = 0;
+    emit signFpsUpdated();
+  }
+
+  _fps_counter++;
+}
+
--- gmlibscenequickfbo.h
+++ gmlibscenequickfbo.h
@@ -9,12 +9,15 @@
 
 class GMlibSceneQuickFbo : public QQuickFramebufferObject {
   Q_OBJECT
+  Q_PROPERTY( unsigned int fps READ fps NOTIFY signFpsUpdated )
+
+  using std_system_clock = std::chrono::system_clock;
+  using std_time_point   = std_system_clock::time_point;
 
 public:
   GMlibSceneQuickFbo();
-
+  
   Renderer*         createRenderer() const override;
-
 
 protected:
   void              keyPressEvent(QKeyEvent *event) override;
@@ -25,12 +28,19 @@ protected:
   void              mouseMoveEvent(QMouseEvent *event) override;
   void              wheelEvent(QWheelEvent *event) override;
 
+private:
+  unsigned int      _fps_avg     {0};
+  unsigned int      _fps_counter {0};
+  std_time_point    _prev_time;
+
+  unsigned int      fps() const;
 
 private slots:
   void              onWindowChanged( QQuickWindow* window );
-
+  void              updateFps();
 
 signals:
+  void              signFpsUpdated();
   void              signKeyPressed( QKeyEvent* event );
   void              signKeyReleased( QKeyEvent* event );
   void              signMouseDoubleClicked( QMouseEvent* event );
